e9f99aa286fce87166b9445c8eba41e2e7336569
SahilC <sahilc.2200@gmail.com>
2017-03-23 19:14:20 +0530

    Circular buffer added for replay

--- replay_mem.py
+++ replay_mem.py
@@ -1,3 +1,21 @@
+import random
+
 Transitions = namedTuple('Transition',('state','action','next_state','reward'))
 
 class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = []
+        self.capacity = capacity
+        self.position = 0
+
+    def push(self, *args):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position += 1
+
+    def sample(self, batch_size):
+        return  random.sample(self.memory,batch_size)
+
+    def __len__(self):
+        return len(self.memory)
